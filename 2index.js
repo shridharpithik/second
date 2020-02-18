ecc174eb1f48dd875b32ee158c3d976ee2a281ad
shridharpithik <59431429+shridharpithik@users.noreply.github.com>
2020-02-18 23:02:06 +0530

    Add files via upload

---
+++ 2index.js
@@ -0,0 +1,15 @@
+var number=prompt("enter number")
+for(var i=1;i<=number*2-3-1-1;i++)
+{
+    var getodd=(2*i-1);
+   
+        document.write(`${getodd} ,`)
+    
+   
+   
+       
+
+    
+   
+
+}
